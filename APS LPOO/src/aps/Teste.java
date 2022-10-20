2fd8a84d7b2930f05cd861d47e71409f7b537450
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-10-20 20:48:34 -0300

    Create Teste.java

---
+++ APS LPOO/src/aps/Teste.java
@@ -0,0 +1,23 @@
+package aps;
+
+import java.util.List;
+
+public class Teste {
+	public static void main(String[] args) {
+		AcessoBancodeDados.testaConnection();
+		//new AcessoBancodeDados().addBooks(new Books("O Medico e o Monstro", "0-13020608-5", 13, 40));
+		
+		AcessoBancodeDados ABD = new AcessoBancodeDados();
+		
+		List<Books> books = ABD.getTodosBooks();
+		
+		for(Books book: books) {
+			System.out.println(book);
+		}
+		
+
+		//new AcessoBancodeDados().addPublishers(new Publishers(0, "Panini", "https://panini.com.br"));
+		//new AcessoBancodeDados().addAuthors(new Authors(0, "Mauricio de Souza", "Osamu Tezuka"));
+	}
+
+}

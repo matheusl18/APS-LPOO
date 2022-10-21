995c15aba290f354d23b5c7fbea2697f41ee60ed
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-10-20 21:33:19 -0300

    Update Teste.java

--- APS LPOO/src/aps/Teste.java
+++ APS LPOO/src/aps/Teste.java
@@ -5,25 +5,36 @@ import java.util.List;
 public class Teste {
 	public static void main(String[] args) {
 		AcessoBancodeDados.testaConnection();
-		//new AcessoBancodeDados().addBooks(new Books("O Medico e o Monstro", "0-13020608-5", 13, 40));
-		
-		AcessoBancodeDados ABD = new AcessoBancodeDados();
-		
-		List<Books> books = ABD.getTodosBooks();
-		
-		for(Books book: books) {
-			System.out.println(book);
-		}
-		
-		List<Authors> authors = ABD.getAuthors();
-		
-		for(Authors author: authors) {
-			System.out.println(author);
-		}
-		
-
+		//new AcessoBancodeDados().addBooks(new Books("Turma da Monica", "0-13020608-5", 13, 40));
+		//new AcessoBancodeDados().addBooksauthors(new Booksauthors("0-13020608-5", 8980, 4));
 		//new AcessoBancodeDados().addPublishers(new Publishers(0, "Panini", "https://panini.com.br"));
 		//new AcessoBancodeDados().addAuthors(new Authors(0, "Mauricio de Souza", "Osamu Tezuka"));
+		
+		AcessoBancodeDados  ABD = new AcessoBancodeDados();
+		
+		//List<Publishers> publishers = ABD.getpublishers();
+		
+		//for(Publishers publisher: publishers) {
+		//	System.out.println(publisher);
+		//}
+		
+		//List<Books> books = ABD.getBooks();
+		
+		//for(Books book: books) {
+		//	System.out.println(book);
+		//}
+		
+		//List<Authors> authors = ABD.getAuthors();
+		
+		//for(Authors author: authors) {
+		//	System.out.println(author);
+		//}
+		
+		//List<Booksauthors> booksauthors = ABD.getBooksauthors();
+		
+		//for(Booksauthors booksauthor: booksauthors) {
+			//System.out.println(booksauthor);
+		//}
 	}
 
 }

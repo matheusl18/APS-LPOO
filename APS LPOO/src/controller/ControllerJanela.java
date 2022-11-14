33443460bbe2a8382a7bd3d8ff7ad963bc57bfd3
matheusl18 <66389666+matheusl18@users.noreply.github.com>
2022-11-14 20:22:54 -0300

    Update ControllerJanela.java

--- APS LPOO/src/controller/ControllerJanela.java
+++ APS LPOO/src/controller/ControllerJanela.java
@@ -2,18 +2,23 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import dao.Dao;
 import entidades.Authors;
-import entidades.Books;
 import entidades.Booksauthors;
 import entidades.Publishers;
+import entidades.Books;
 import view.janela.ViewJFrame;
 
 public class ControllerJanela {
     
     Dao model;
     ViewJFrame view;
+    private Publishers publishers;
+	private Authors authors;
+	private Books books;
+	private Booksauthors booksauthors;
     
     public ControllerJanela(Dao model, ViewJFrame view){
         this.model = model;
@@ -21,6 +26,11 @@ public class ControllerJanela {
     }
     
     public void init(){
+    	view.addActionListarPublishers(new AcaoListarPublishers());
+    	view.addActionListarAuthors(new AcaoListarAuthors());
+        view.addActionListarBooks(new AcaoListarBooks());
+        view.addActionListarBooksauthors(new AcaoListarBooksauthors());
+    	
     	view.addActionAdicionarPublishers(new AcaoAdicionaPublishers());
         view.addActionAdicionarAuthors(new AcaoAdicionaAuthors());
         view.addActionAdicionarBooks(new AcaoAdicionaBooks());
@@ -35,7 +45,101 @@ public class ControllerJanela {
         view.addActionUpdateAuthors(new AcaoUpdateAuthors());
         view.addActionUpdateBooks(new AcaoUpdateBooks());
         view.addActionUpdateBooksauthors(new AcaoUpdateBooksauthors());
+        
+        view.addActionBuscaPublishers(new AcaoBuscaPublishers());
+        view.addActionBuscaAuthors(new AcaoBuscaAuthors());
+        view.addActionBuscaBooks(new AcaoBuscaBooks());
+        view.addActionBuscaBooksauthors(new AcaoBuscaBooksauthors());
     }
+    
+    class AcaoListarPublishers implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	List<Publishers> publishers;
+        	publishers = model.getPublishers();
+        	view.listarPublishers(publishers);
+        }
+    }
+    class AcaoListarAuthors implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	List<Authors> authors;
+        	authors = model.getAuthors();
+        	view.listarAuthors(authors);
+        }
+    }
+    class AcaoListarBooks implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	List<Books> books;
+        	books = model.getBooks();
+        	view.listarBooks(books);
+        }
+    }
+    class AcaoListarBooksauthors implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	List<Booksauthors> booksauthors;
+        	booksauthors = model.getBooksauthors();
+        	view.listarBooksauthors(booksauthors);
+        }
+    }
+    
+    class AcaoBuscaPublishers implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+	         String nome = view.getKeyPublishersBusca();
+	       	 
+	       	 List<Publishers> publishers = model.getPublishers();
+	       	 publishers = model.buscaPublishers(nome);
+	       	 
+	       	 view.srcPublishers(publishers);
+        }
+    }
+    class AcaoBuscaAuthors implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+        	 String nome = view.getKeyNameAuthorsBusca();
+        	 String fnome = view.getKeyFnameAuthorsBusca();
+        	 
+        	 List<Authors> authors ;
+        	 authors = model.buscaAuthors(nome, fnome);
+        	 
+        	 view.srcAuthors(authors);
+        }
+    }
+    class AcaoBuscaBooks implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+	         String title = view.getKeyBooksBusca();
+	       	 
+	       	 List<Books> books = model.getBooks();
+	       	 books = model.buscaBooks(title);
+	       	 
+	       	 view.srcBooks(books);
+        }
+    }
+    class AcaoBuscaBooksauthors implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+	         String isbn = view.getKeyBooksauthorsBusca();
+	       	 
+	       	 List<Booksauthors> booksauthorsauthors = model.getBooksauthors();
+	       	 booksauthorsauthors = model.buscaBooksauthors(isbn);
+	       	 
+	       	 view.srcBooksauthors(booksauthorsauthors);
+        }
+    }
+    
+    
     
     class AcaoAdicionaPublishers implements ActionListener{
 
